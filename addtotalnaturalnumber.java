fc26365678cfaabba75875ef966303cddbee9c63
ravikantroushan <125126526+ravikantroushan@users.noreply.github.com>
2023-02-27 21:41:08 +0530

    Add files via upload

---
+++ addtotalnaturalnumber.java
@@ -0,0 +1,25 @@
+public class addtotalnaturalnumber {
+
+        public static void main(String[] args) {
+//        int sum =0;
+//        for (int i = 1; i<=4; i++){
+//
+//            sum = sum + i;
+//            System.out.println(sum);
+//
+//
+//        }
+        /*int num =0;
+        for (int i =1; i<=4; i++){
+            num = num + i;
+        }
+        System.out.println(num);
+*/
+//        for ( int j=1; j<=4; j++){
+//            int om = 0;
+//            om = om + j;
+//            System.out.println(om);
+//        }
+        }
+    }
+

6580be4cf0b447e3fc3c36200084cd1d76771fe8
ravikantroushan <125126526+ravikantroushan@users.noreply.github.com>
2023-03-13 10:15:46 +0530

    Add files via upload

---
+++ forLoopRecap.java
@@ -0,0 +1,9 @@
+public class forLoopRecap {
+    public static void main(String[] args) {
+        for (int i = 100; i >0 ; i+=10) {
+            System.out.println("number is "+ i);
+            // endless loop because here condition is always true.
+            
+        }
+    }
+}
